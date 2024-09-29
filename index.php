0f889382b677763411372c4ca7d13a60cafe25f9
SYLG <2330180633@qq.com>
2024-09-29 19:56:31 +0800

    [feat] set cookie when check or uncheck fields

--- index.php
+++ index.php
@@ -16,6 +16,11 @@ $all_fields = getAllFields();
 
 $default_show_fields = ['title', 'authors', 'journal', 'year', 'tags'];
 
+// 检查是否存在设置显示字段的GET请求
+if (!empty($show_fields)) {
+    setcookie('selectedFields', json_encode($show_fields), time() + (86400 * 30), "/"); // 设置cookie，有效期30天
+}
+
 if (empty($show_fields) && isset($_COOKIE['selectedFields'])) {
     $show_fields = json_decode($_COOKIE['selectedFields'], true);
 }
